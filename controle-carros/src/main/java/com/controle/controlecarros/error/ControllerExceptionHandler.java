e126b21ed52d1bbe2187db10cc6ef5f48672e858
wesleytoledo05 <vntoledo05@gmail.com>
2021-05-18 22:49:48 -0300

    Ajustando mensagens de erro

---
+++ controle-carros/src/main/java/com/controle/controlecarros/error/ControllerExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.controle.controlecarros.error;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  @ResponseStatus(value = HttpStatus.NOT_FOUND)
+  public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+    ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND.value(), new Date(), ex.getMessage(),
+        request.getDescription(false));
+
+    return message;
+  }
+
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+  public ErrorMessage globalExceptionHandler(Exception ex, WebRequest request, ErrorMessage errorMessage) {
+    ErrorMessage message = new ErrorMessage(HttpStatus.BAD_REQUEST.value(), new Date(), "ALALALAL"  ,
+        request.getDescription(false));
+    System.out.println("EX"+ex.getMessage());
+    System.out.println("ERRMESS"+errorMessage);
+
+    
+    return message;
+  }
+
+  
+
+
+}
